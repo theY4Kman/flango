29bca4fa0ac27163ef05f808f4e313541a0148eb
Zach Kanzler <zkanzler@perchsecurity.com>
2018-04-26 09:18:42 -0400

    Version 0.0.2: show README.md on PyPI.org

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased]
+
+
+## [0.0.2] - 2018-04-26
+### Changed
+ - Display Markdown README on fancy new PyPI.org
+
+
+## [0.0.1] - 2017-02-11
+ - Initial release
--- setup.py
+++ setup.py
@@ -3,19 +3,31 @@ import os
 from setuptools import setup
 
 
-requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-with open(requirements_path) as fp:
-    requirements = list(fp)
+ROOT_DIR = os.path.dirname(__file__)
+requirements_path = os.path.join(ROOT_DIR, 'requirements.txt')
+readme_path = os.path.join(ROOT_DIR, 'README.md')
+
+
+def get_requirements():
+    with open(requirements_path) as fp:
+        return list(fp)
+
+
+def get_readme():
+    with open(readme_path) as fp:
+        return fp.read()
 
 
 setup(
     name='flango',
-    version='0.0.1',
+    version='0.0.2',
     packages=['flango'],
-    install_requires=requirements,
+    install_requires=get_requirements(),
     url='https://github.com/theY4Kman/flango',
     license='MIT',
     author='Zach "theY4Kman" Kanzler',
     author_email='they4kman@gmail.com',
-    description='Flask-like interface for Django'
+    description='Flask-like interface for Django',
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
 )
